80f68dd9efab7f35522daf127f36f633e6a022ef
Luis <luis@vinofilos.es>
2026-06-28 21:44:57 +0000

    fix(front): render products with PrestaShop miniature template

--- services/BrandSeoProductService.php
+++ services/BrandSeoProductService.php
@@ -1,51 +1,45 @@
 <?php
 
-require_once _PS_MODULE_DIR_.'brandseo/repositories/BrandSeoProductRepository.php';
-
 class BrandSeoProductService
 {
-    private $repository;
-
-    public function __construct()
-    {
-        $this->repository = new BrandSeoProductRepository();
-    }
-
     public function getLandingProducts($idManufacturer, $idLang)
     {
-        $products = $this->repository->getProductsByManufacturer($idManufacturer, $idLang, 12);
-        $link = Context::getContext()->link;
+        $assembler = new ProductAssembler(Context::getContext());
+        $presenterFactory = new ProductPresenterFactory(Context::getContext());
+        $presentationSettings = $presenterFactory->getPresentationSettings();
 
-        foreach ($products as &$product) {
-            $idProduct = (int) $product['id_product'];
+        $presenter = new ProductListingPresenter(
+            new ImageRetriever(Context::getContext()->link),
+            Context::getContext()->link,
+            new PriceFormatter(),
+            new ProductColorsRetriever(),
+            Context::getContext()->getTranslator()
+        );
 
-            $product['url'] = $link->getProductLink($idProduct, $product['link_rewrite']);
-            $product['image_url'] = '';
+        $rows = Db::getInstance()->executeS('
+            SELECT p.id_product
+            FROM `'._DB_PREFIX_.'product` p
+            INNER JOIN `'._DB_PREFIX_.'product_shop` ps
+                ON ps.id_product = p.id_product
+                AND ps.id_shop = '.(int) Context::getContext()->shop->id.'
+            WHERE p.id_manufacturer = '.(int) $idManufacturer.'
+            AND ps.active = 1
+            ORDER BY p.date_add DESC
+            LIMIT 12
+        ');
 
-            if (!empty($product['id_image'])) {
-                $product['image_url'] = $link->getImageLink(
-                    $product['link_rewrite'],
-                    $idProduct.'-'.(int) $product['id_image'],
-                    ImageType::getFormattedName('home')
-                );
-            }
+        $products = array();
 
-            $price = Product::getPriceStatic($idProduct, true);
-            $product['price_formatted'] = Tools::displayPrice($price);
+        foreach ($rows as $row) {
+            $rawProduct = array('id_product' => (int) $row['id_product']);
+            $assembledProduct = $assembler->assembleProduct($rawProduct);
 
-            $product['add_to_cart_url'] = $link->getPageLink(
-                'cart',
-                true,
-                null,
-                array(
-                    'add' => 1,
-                    'id_product' => $idProduct,
-                    'token' => Tools::getToken(false),
-                )
+            $products[] = $presenter->present(
+                $presentationSettings,
+                $assembledProduct,
+                Context::getContext()->language
             );
         }
-
-        unset($product);
 
         return $products;
     }
--- views/templates/front/partials/products.tpl
+++ views/templates/front/partials/products.tpl
@@ -3,44 +3,11 @@
 
     <div class="brandseo-products-v2-header">
         <h2>Vinos de {$manufacturer->name|escape:'html':'UTF-8'}</h2>
-        <a href="#brand-products">Ver todos los vinos</a>
     </div>
 
-    <div class="brandseo-products-v2-grid">
+    <div class="brandseo-prestashop-products">
         {foreach from=$brand_products item=product}
-            <article class="brandseo-product-v2-card">
-
-                <a class="brandseo-product-v2-image" href="{$product.url|escape:'html':'UTF-8'}">
-                    {if $product.image_url}
-                        <img src="{$product.image_url|escape:'html':'UTF-8'}" alt="{$product.name|escape:'html':'UTF-8'}" loading="lazy">
-                    {else}
-                        <span>Sin imagen</span>
-                    {/if}
-                </a>
-
-                <div class="brandseo-product-v2-body">
-                    <h3>
-                        <a href="{$product.url|escape:'html':'UTF-8'}">
-                            {$product.name|escape:'html':'UTF-8'}
-                        </a>
-                    </h3>
-
-                    <div class="brandseo-product-v2-price">
-                        {$product.price_formatted|escape:'html':'UTF-8'}
-                    </div>
-
-                    <div class="brandseo-product-v2-actions">
-                        <a class="brandseo-product-v2-cart" href="{$product.add_to_cart_url|escape:'html':'UTF-8'}">
-                            Añadir al carrito
-                        </a>
-
-                        <a class="brandseo-product-v2-more" href="{$product.url|escape:'html':'UTF-8'}">
-                            ♥
-                        </a>
-                    </div>
-                </div>
-
-            </article>
+            {include file='catalog/_partials/miniatures/product.tpl' product=$product}
         {/foreach}
     </div>
 
