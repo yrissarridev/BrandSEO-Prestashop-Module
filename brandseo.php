364bd189acc60b639621e20f3b9ab0cbbc7e401c
Luis <luis@vinofilos.es>
2026-06-28 08:38:21 +0000

    refactor(installer): extract database installer

--- brandseo.php
+++ brandseo.php
@@ -4,6 +4,7 @@ if (!defined('_PS_VERSION_')) {
 }
 
 require_once __DIR__.'/classes/BrandSeoLanding.php';
+require_once __DIR__.'/helpers/BrandSeoInstaller.php';
 
 class Brandseo extends Module
 {
@@ -31,7 +32,7 @@ class Brandseo extends Module
     public function install()
     {
         return parent::install()
-            && $this->installSql()
+            && BrandSeoInstaller::installDatabase()
             && $this->installAdminTabs()
             && $this->registerHook('displayBackOfficeHeader');
     }
@@ -39,7 +40,7 @@ class Brandseo extends Module
     public function uninstall()
     {
         return $this->uninstallAdminTabs()
-            && $this->uninstallSql()
+            && BrandSeoInstaller::uninstallDatabase()
             && parent::uninstall();
     }
 
@@ -53,69 +54,6 @@ class Brandseo extends Module
 
         $this->context->controller->addCSS($this->_path.'views/css/admin.css');
         $this->context->controller->addJS($this->_path.'views/js/admin.js');
-    }
-
-    private function installSql()
-    {
-        $queries = array();
-
-        $queries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'brandseo_landing` (
-            `id_brandseo_landing` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-            `id_manufacturer` INT UNSIGNED DEFAULT NULL,
-            `type` VARCHAR(32) NOT NULL DEFAULT "brand",
-            `slug` VARCHAR(255) NOT NULL,
-            `status` VARCHAR(32) NOT NULL DEFAULT "draft",
-            `active` TINYINT(1) NOT NULL DEFAULT 0,
-            `noindex` TINYINT(1) NOT NULL DEFAULT 1,
-            `redirect_enabled` TINYINT(1) NOT NULL DEFAULT 0,
-            `hero_image` VARCHAR(255) DEFAULT NULL,
-            `logo_image` VARCHAR(255) DEFAULT NULL,
-            `latitude` DECIMAL(10,7) DEFAULT NULL,
-            `longitude` DECIMAL(10,7) DEFAULT NULL,
-            `website` VARCHAR(255) DEFAULT NULL,
-            `instagram` VARCHAR(255) DEFAULT NULL,
-            `facebook` VARCHAR(255) DEFAULT NULL,
-            `youtube` VARCHAR(255) DEFAULT NULL,
-            `country` VARCHAR(128) DEFAULT NULL,
-            `region` VARCHAR(128) DEFAULT NULL,
-            `date_add` DATETIME NOT NULL,
-            `date_upd` DATETIME NOT NULL,
-            PRIMARY KEY (`id_brandseo_landing`),
-            KEY `idx_manufacturer` (`id_manufacturer`),
-            KEY `idx_type` (`type`),
-            KEY `idx_slug` (`slug`),
-            KEY `idx_status` (`status`)
-        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
-
-        $queries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'brandseo_landing_lang` (
-            `id_brandseo_landing` INT UNSIGNED NOT NULL,
-            `id_lang` INT UNSIGNED NOT NULL,
-            `title` VARCHAR(255) DEFAULT NULL,
-            `h1` VARCHAR(255) DEFAULT NULL,
-            `meta_title` VARCHAR(255) DEFAULT NULL,
-            `meta_description` VARCHAR(512) DEFAULT NULL,
-            `excerpt` TEXT DEFAULT NULL,
-            `history` MEDIUMTEXT DEFAULT NULL,
-            `philosophy` MEDIUMTEXT DEFAULT NULL,
-            `store_opinion` MEDIUMTEXT DEFAULT NULL,
-            PRIMARY KEY (`id_brandseo_landing`, `id_lang`)
-        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
-
-        foreach ($queries as $query) {
-            if (!Db::getInstance()->execute($query)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private function uninstallSql()
-    {
-        Db::getInstance()->execute('DROP TABLE IF EXISTS `'._DB_PREFIX_.'brandseo_landing_lang`');
-        Db::getInstance()->execute('DROP TABLE IF EXISTS `'._DB_PREFIX_.'brandseo_landing`');
-
-        return true;
     }
 
     private function installAdminTabs()
---
+++ helpers/BrandSeoInstaller.php
@@ -0,0 +1,67 @@
+<?php
+
+class BrandSeoInstaller
+{
+    public static function installDatabase()
+    {
+        $queries = array();
+
+        $queries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'brandseo_landing` (
+            `id_brandseo_landing` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+            `id_manufacturer` INT UNSIGNED DEFAULT NULL,
+            `type` VARCHAR(32) NOT NULL DEFAULT "brand",
+            `slug` VARCHAR(255) NOT NULL,
+            `status` VARCHAR(32) NOT NULL DEFAULT "draft",
+            `active` TINYINT(1) NOT NULL DEFAULT 0,
+            `noindex` TINYINT(1) NOT NULL DEFAULT 1,
+            `redirect_enabled` TINYINT(1) NOT NULL DEFAULT 0,
+            `hero_image` VARCHAR(255) DEFAULT NULL,
+            `logo_image` VARCHAR(255) DEFAULT NULL,
+            `latitude` DECIMAL(10,7) DEFAULT NULL,
+            `longitude` DECIMAL(10,7) DEFAULT NULL,
+            `website` VARCHAR(255) DEFAULT NULL,
+            `instagram` VARCHAR(255) DEFAULT NULL,
+            `facebook` VARCHAR(255) DEFAULT NULL,
+            `youtube` VARCHAR(255) DEFAULT NULL,
+            `country` VARCHAR(128) DEFAULT NULL,
+            `region` VARCHAR(128) DEFAULT NULL,
+            `date_add` DATETIME NOT NULL,
+            `date_upd` DATETIME NOT NULL,
+            PRIMARY KEY (`id_brandseo_landing`),
+            KEY `idx_manufacturer` (`id_manufacturer`),
+            KEY `idx_type` (`type`),
+            KEY `idx_slug` (`slug`),
+            KEY `idx_status` (`status`)
+        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
+
+        $queries[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'brandseo_landing_lang` (
+            `id_brandseo_landing` INT UNSIGNED NOT NULL,
+            `id_lang` INT UNSIGNED NOT NULL,
+            `title` VARCHAR(255) DEFAULT NULL,
+            `h1` VARCHAR(255) DEFAULT NULL,
+            `meta_title` VARCHAR(255) DEFAULT NULL,
+            `meta_description` VARCHAR(512) DEFAULT NULL,
+            `excerpt` TEXT DEFAULT NULL,
+            `history` MEDIUMTEXT DEFAULT NULL,
+            `philosophy` MEDIUMTEXT DEFAULT NULL,
+            `store_opinion` MEDIUMTEXT DEFAULT NULL,
+            PRIMARY KEY (`id_brandseo_landing`, `id_lang`)
+        ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
+
+        foreach ($queries as $query) {
+            if (!Db::getInstance()->execute($query)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static function uninstallDatabase()
+    {
+        Db::getInstance()->execute('DROP TABLE IF EXISTS `'._DB_PREFIX_.'brandseo_landing_lang`');
+        Db::getInstance()->execute('DROP TABLE IF EXISTS `'._DB_PREFIX_.'brandseo_landing`');
+
+        return true;
+    }
+}
